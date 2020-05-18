69ce4ddd012b2ca1d63a42f96294f50057ace4fe
vikrant0526 <60457380+vikrant0526@users.noreply.github.com>
2020-05-18 23:34:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <meta name="author" content="potenzaglobalsolutions.com" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <title>Webster - Responsive Multi-purpose HTML5 Template</title>
-<!-- Vikrant bhadva h-->
+<!-- Ricky Reti Randi -->
 <!-- <i class="bx bx-comment"></i> -->
 <!-- Favicon -->
 <link rel="shortcut icon" href="images/favicon.ico" />
