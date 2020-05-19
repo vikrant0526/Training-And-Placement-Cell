647b2d758b6b9a74c6f246fef80c6ad05bde8973
Manav Shah <45305963+manav3475@users.noreply.github.com>
2020-05-19 19:14:01 +0530

    sample

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="potenzaglobalsolutions.com" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <title>Webster - Responsive Multi-purpose HTML5 Template</title>
-    <!-- Ricky Reti Randi -->
+    <!-- Vikrant Lodu Chodu -->
     <!-- <i class="bx bx-comment"></i> -->
     <!-- Favicon -->
     <link rel="shortcut icon" href="images/favicon.ico" />
