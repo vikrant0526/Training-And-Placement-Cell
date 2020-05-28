bc460030c05208d7ee6cb433494d95d4e2ff27a0
vikrant0526 <60457380+vikrant0526@users.noreply.github.com>
2020-05-28 20:30:41 +0530

    Tried and failed to rework ckeditor

--- Faculty/Committee_Head/broadcast_sender.php
+++ Faculty/Committee_Head/broadcast_sender.php
@@ -18,32 +18,32 @@
   $stmt->bindparam(":blid", $blid);
   $stmt->execute();
 ?>
-    <script src="../../Files/ckeditor/ckeditor.js"></script>
-    <!-- <script src="../../Files/ckeditor5/build/ckeditor.js"></script> -->
-    <div class="content-wrapper header-info">
-      <!-- widgets -->
-      <div class="mb-30">
-           <div class="card h-100 ">
-           <div class="card-body h-100">
-             <h4 class="card-title">Send Broadcast</h4>
-             <!-- action group -->
-             <div class="scrollbar">
-              <ul class="list-unstyled">
-                <div id="sent_1">
-                  <li>
-                    <div class="media">
-                      <div class="media-body mb-2">
-                        <div id="sent_1"></div>
-                      </div>
-                    </div>
-                  </li>
+<script src="../../Files/ckeditor/ckeditor.js"></script>
+<!-- <script src="../../Files/ckeditor5/build/ckeditor.js"></script> -->
+<div class="content-wrapper header-info">
+    <!-- widgets -->
+    <div class="mb-30">
+        <div class="card h-100 ">
+            <div class="card-body h-100">
+                <h4 class="card-title">Send Broadcast</h4>
+                <!-- action group -->
+                <div class="scrollbar">
+                    <ul class="list-unstyled">
+                        <div id="sent_1">
+                            <li>
+                                <div class="media">
+                                    <div class="media-body mb-2">
+                                        <div id="sent_1"></div>
+                                    </div>
+                                </div>
+                            </li>
+                        </div>
+                    </ul>
                 </div>
-              </ul>
-             </div>
             </div>
-          </div>
         </div>
-<?php 
+    </div>
+    <?php 
   include('footer.php');
   $cnt = 0;
   while ($data1 = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -61,17 +61,17 @@
     setcookie("email".$cnt, $data1['COMPANY_EMAIL'], time() + (10), "/");
 
     ?>
-      <script type="text/javascript">
-        function send(){
-          var xmlhttp=new XMLHttpRequest();
-          xmlhttp.open("GET","message_send.php?id=<?php echo $data1['COMPANY_ID']; ?>&cnt=<?php echo $cnt; ?>",false);
-          xmlhttp.send(null);
-          var i = <?php echo json_encode($cnt); ?>;
-          var div_id = "sent_".concat(i);
-          document.getElementById(div_id).innerHTML=xmlhttp.responseText;
-        }
-        send();
-      </script>
+    <script type="text/javascript">
+    function send() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", "message_send.php?id=<?php echo $data1['COMPANY_ID']; ?>&cnt=<?php echo $cnt; ?>", false);
+        xmlhttp.send(null);
+        var i = < ? php echo json_encode($cnt); ? > ;
+        var div_id = "sent_".concat(i);
+        document.getElementById(div_id).innerHTML = xmlhttp.responseText;
+    }
+    send();
+    </script>
     <?php
   }
 ?>
