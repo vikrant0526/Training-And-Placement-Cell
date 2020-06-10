01f71480e16839370d721b2eb6873762ae894ef0
Manav Shah <45305963+manav3475@users.noreply.github.com>
2020-06-10 21:00:49 +0530

    FPDF Package Updated

--- Faculty/Committee_Head/placement_schedule_download.php
+++ Faculty/Committee_Head/placement_schedule_download.php
@@ -1,16 +1,19 @@
 <?php 
     require "../../Files/fpdf.php";
     $con =  new PDO("mysql:host=localhost;dbname=ntpcell","root","");	
-    class myPDF extends FPDF{
-            function header(){
+    class myPDF extends FPDF{    
+        function header(){
+                // $new_year=mktime(0,0,0,date('m'),date('d'),date('Y')-1);
                 $this->Image('../../Files/images/logo-mini.png',10,6);
                 $this->SetFont('Helvetica','B',45);
                 $this->Cell(320,20,'PLACEMENT SCHEDULE',0,0,'C');
                 $this->Ln();
                 $this->SetFont('Helvetica','B',25);
-                $this->Cell(276,10,'For '.date('Y'),0,0,'C');
+                $this->Cell(276,10,'Academic Year '.date('Y'),0,0,'C');
                 $this->Ln();
-                
+                $this->Ln();
+                $this->Ln();
+                $this->Ln();
             }
             
             function footer(){
@@ -21,20 +24,24 @@
 
             function headerTable(){
                 $this->SetFont('Times','B',12);
-                $this->Cell(60,10,'COMPANY NAME',1,0,'C');
-                $this->Cell(80,10,'START DATE',1,0,'C');
-                $this->Cell(36,10,'END DATE',1,0,'C');
+                $this->Cell(40,10,'Sr. No',1,0,'C');
+                $this->Cell(100,10,'COMPANY NAME',1,0,'C');
+                $this->Cell(70,10,'START DATE',1,0,'C');
+                $this->Cell(70,10,'END DATE',1,0,'C');
                 $this->Ln();
             }
 
             function viewTable($con){
-                $this->SetFont('Times','B',12);
+                $this->SetFont('Arial','I',12);
                 $stmt=$con->prepare("CALL GET_PLACMENT_SCHEDULE()");
                 $stmt->execute();
+                $a=0;
                 while ($x=$stmt->fetch(PDO::FETCH_OBJ)) {
-                    $this->Cell(60,10,$x->COMPANY_NAME,1,0,'C');
-                    $this->Cell(80,10,$x->PLACEMENT_SCHEDULE_START_DATE,1,0,'L');
-                    $this->Cell(36,10,$x->PLACEMENT_SCHEDULE_END_DATE,1,0,'L');
+                    $a+=1;
+                    $this->Cell(40,10,$a,1,0,'C');
+                    $this->Cell(100,10,$x->COMPANY_NAME,1,0,'C');
+                    $this->Cell(70,10,$x->PLACEMENT_SCHEDULE_START_DATE,1,0,'C');
+                    $this->Cell(70,10,$x->PLACEMENT_SCHEDULE_END_DATE,1,0,'C');
                     $this->Ln();
                 }
             }
