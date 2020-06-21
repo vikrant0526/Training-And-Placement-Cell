77e7c5930be3a8b03c3dcae53d51eb97c6f6cafd
Manav Shah <45305963+manav3475@users.noreply.github.com>
2020-06-21 16:38:52 +0530

    Update deny.php

--- Student/deny.php
+++ Student/deny.php
@@ -1,8 +1,6 @@
 <?php	
 	$nid = $_GET['nid'];
-	
 	include('../Files/PDO/dbcon.php');
-
 	$stmt=$con->prepare(" CALL REMOVE_NOTIFICATION(:nid);");
     $stmt->bindparam(":nid",$nid);
     $stmt->execute();
